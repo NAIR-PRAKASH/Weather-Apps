7e3f2e84f25faf8612f16bf0150100a937dad824
NAIR_PRAKASH <nairprakash4540@gmail.com>
2025-12-12 21:16:13 +0000

    Fix case-sensitive import paths

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -7,7 +7,7 @@ export default function SearchBar({onSearch}){
     <div className="mt-20">
       <div className="flex shadow-lg rounded overflow-hidden">
         <input value={city} onChange={e=>setCity(e.target.value)}
-        className="px-4 py-2 w-70 mt-0 text-black" placeholder="Enter city"/><br/>
+        className="px-4 py-2 w-70 mt-0 text-blue-50" placeholder="Enter city"/><br/>
 
         <button onClick={()=>{if(city)onSearch(city);}}
           className="bg-blue-700 px-4 py-2 mt-0 hover:bg-amber-400 text-white cursor-pointer">Search</button>
